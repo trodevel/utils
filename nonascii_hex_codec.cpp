fef0a3bfea8a3fdc0f559c72e538eba8c65a5a09
trodevel <trodevel@gmail.com>
2017-03-22 01:41:44 +0100

    sync with svn

---
+++ nonascii_hex_codec.cpp
@@ -0,0 +1,91 @@
+/*
+
+NON-ASCII HEX codec.
+
+Copyright (C) 2015 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 6124 $ $Date:: 2017-03-20 #$ $Author: serge $
+
+#include "nonascii_hex_codec.h"         // self
+
+#include <boost/algorithm/hex.hpp>      // boost::unhex
+
+namespace utils
+{
+
+namespace nonascii_hex_codec
+{
+
+std::string decode( const std::string & s )
+{
+    std::string res;
+
+    auto size = s.size();
+
+    if( size == 1 )
+    {
+        // empty string
+        if( s == "=" )
+            return res;
+    }
+
+    for( std::string::size_type i = 0; i < size; ++i )
+    {
+        auto c = s[i];
+
+        if( c == '=' &&  ( i + 3 < size ) )
+        {
+            res += boost::algorithm::unhex( s.substr( i + 1, 2 ) );
+            i += 2;
+        }
+        else
+            res += c;
+    }
+
+    return res;
+
+    return boost::algorithm::unhex( s );
+}
+
+std::string encode( const std::string & s )
+{
+    std::string res;
+
+    if( s.empty() )
+    {
+        // empty string
+        return std::string( "=" );
+    }
+
+    for( auto & c : s )
+    {
+        if( c < 33 || c > 126 || c == '=' )
+        {
+            res += "=";
+            (void) boost::algorithm::detail::encode_one( c, std::back_inserter( res ));
+        }
+        else
+            res += c;
+    }
+
+    return res;
+}
+
+}
+
+} // namespace utils
---
+++ nonascii_hex_codec.h
@@ -0,0 +1,42 @@
+/*
+
+NON-ASCII HEX codec.
+
+Copyright (C) 2015 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 6124 $ $Date:: 2017-03-20 #$ $Author: serge $
+
+#ifndef LIB_UTILS_NONASCII_HEX_CODEC_H
+#define LIB_UTILS_NONASCII_HEX_CODEC_H
+
+#include <string>
+
+namespace utils
+{
+
+namespace nonascii_hex_codec
+{
+
+std::string decode( const std::string & s );
+std::string encode( const std::string & s );
+
+}
+
+} // namespace utils
+
+#endif // LIB_UTILS_NONASCII_HEX_CODEC_H
