8be47b58c18cf045e5d201b1cfffaabffef15441
trodevel <trodevel@gmail.com>
2018-05-15 00:27:20 +0200

    added COPY_MEMBER

---
+++ copy_member.h
@@ -0,0 +1,29 @@
+/*
+
+AUX macro.
+
+Copyright (C) 2018 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 9173 $ $Date:: 2018-05-14 #$ $Author: serge $
+
+#ifndef UTILS__COPY_MEMBER_H
+#define UTILS__COPY_MEMBER_H
+
+#define COPY_MEMBER( _d, _s, _m )     { _d->_m=_s._m; }
+
+#endif // UTILS__COPY_MEMBER_H
